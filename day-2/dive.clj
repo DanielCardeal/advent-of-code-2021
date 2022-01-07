a223c41ebf38b552422e37237612dee37c672878
Daniel Pessoa Cardeal <cardeal.daniel@usp.br>
2022-01-07 01:15:40 -0300

    (2.1) Resolve desafio ~de um jeito farofa~.

--- day-2/dive.clj
+++ day-2/dive.clj
@@ -12,3 +12,24 @@
          (map #(str/split % #"\s+"))
          (map #(list (str->dir (first %))
                      (Integer/parseInt (second %)))))))
+
+;; --- Parte 1
+(defn dir_displacement
+  "Get the total displacement for a given direction."
+  [dir dirs]
+  (->> (filter #(= dir (first %)) dirs)
+       (map second)
+       (apply +)))
+
+(defn tot_displacement
+  "Calculate the total displacement of the submarine based on a list of
+  directions."
+  [dirs]
+  (let [horiz (dir_displacement :forward dirs)
+        depth (- (dir_displacement :down dirs)
+                 (dir_displacement :up dirs))]
+    {:horiz horiz, :depth depth}))
+
+;; Resposta
+(let [{h :horiz d :depth} (tot_displacement directions)]
+  (* h d))
